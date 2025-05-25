4b365cf958fd7a9c68c66b9838ffdc0d3dffad97
Dimo Dimov <djdimcho25@gmail.com>
2025-05-25 12:54:28 +0300

    add folders

---
+++ app/(admin)/admin/layout.js
@@ -0,0 +1,20 @@
+import { getAdmin } from '@/actions/admin'
+import Header from '@/components/Header';
+import { notFound } from 'next/navigation';
+import React from 'react'
+
+const AdminLayout = async() => {
+
+    const admin = await getAdmin();
+
+    if(!admin.authorized){
+        return notFound();
+    }
+  return (
+    <div className='h-full'>
+        <Header isAdminPage={true} />
+    </div>
+  )
+}
+
+export default AdminLayout
